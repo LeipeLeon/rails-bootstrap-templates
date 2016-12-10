7115b04cfe178514996054ad5d9491e87654832f
Leon Berenschot <leipeleon@gmail.com>
2016-12-10 11:37:57 +0100

    Put the templates path onto the beginning of the array of template paths.
    h/t: http://stackoverflow.com/questions/5666595/how-to-override-a-rails-generator-template-in-a-gem

--- lib/rails/bootstrap/templates.rb
+++ lib/rails/bootstrap/templates.rb
@@ -3,7 +3,11 @@ require "rails/bootstrap/templates/version"
 module Rails
   module Bootstrap
     module Templates
-      # Your code goes here...
+      class Railtie < Rails::Railtie
+        config.app_generators do |g|
+          g.templates.unshift File::expand_path('../../../templates', __FILE__)
+        end
+      end
     end
   end
 end
